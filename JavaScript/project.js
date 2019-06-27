4aec49c71f99e3d81d2d15cc4bdeaaa6212f080c
Kaayathiri <kaayathiri_11@hotmail.co.uk>
2019-06-27 16:57:23 +0100

    connect

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -7,17 +7,16 @@ xhr.responseType = 'text';
 xhr.onload = function() {
    if (xhr.status ===200) {
    var activityInfo = JSON.parse(xhr.responseText);
-   console.log(activityInfo);
+ //  console.log(activityInfo);
 
-//     console.log(activityInfo[0].ingredients);
-//    } 
+
 
 var myActivityInfo = "";
    for (i=0; i<activityInfo.length; i++) {
        var x=i+1;
    
-       myActivityInfo += "Activity Name: "+activityInfo[i].activity_name+" <br>";
-       myActivityInfo += "Information: "+activityInfo[i].activity_information+" <br>";
+       myActivityInfo += "Activity Name: <br>"+activityInfo[i].activity_name+" <br>";
+       myActivityInfo += "Information: <br>"+activityInfo[i].activity_information+" <br>";
    } // end loop
 
    document.getElementById('message').innerHTML = myActivityInfo;
